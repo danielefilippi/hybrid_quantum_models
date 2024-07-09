af065e0ab90f840be9b771fd3a1f3c1b85959139
danielefilippi <d.filippi6@studenti.unipi.it>
2024-07-09 15:42:24 +0200

    Update hcnn_quanv_2.py

--- hqm/classification/hcnn_quanv_2.py
+++ hqm/classification/hcnn_quanv_2.py
@@ -6,7 +6,7 @@ from hqm.utils.sizes import size_conv_layer
 from hqm.layers.quanvolution import Quanvolution2D
 import torch
 
-class HybridLeNet5(torch.nn.Module):
+class HybridLeNet5_quanv_2(torch.nn.Module):
     '''
         This class implements a quantum hybrid convolutional neural network based on LeNet-5.
         HybridLeNet5 is composed of classical convlutional block and hybrid quantum MLP.
